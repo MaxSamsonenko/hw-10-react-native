bd0ceb97b654506581d3a585afe96d20fdfe63c1
Max Samsonenko <samsonenko.maxwork@gmail.com>
2024-10-21 19:20:45 +0300

    background tag added

--- screens/RegistrationScreen.tsx
+++ screens/RegistrationScreen.tsx
@@ -1,33 +1,29 @@
 import { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Image, Text, TextInput, TouchableOpacity } from "react-native";
 
 const RegistrationScreen: React.FC = () => {
 	const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
 
 	return (
 		<View>
+			<Image /> //background
 			<View>
 				<TouchableOpacity>
 					<Text>+</Text>
 				</TouchableOpacity>
 			</View>
-
 			<Text>Реєстрація</Text>
-
 			<TextInput placeholder="Логін" />
 			<TextInput placeholder="Адреса електронної пошти" />
-
 			<View>
 				<TextInput placeholder="Пароль" secureTextEntry={!passwordVisible} />
 				<TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
 					<Text>{passwordVisible ? "Сховати" : "Показати"}</Text>
 				</TouchableOpacity>
 			</View>
-
 			<TouchableOpacity>
 				<Text>Зареєструватися</Text>
 			</TouchableOpacity>
-
 			<TouchableOpacity>
 				<Text>Вже є акаунт? Увійти</Text>
 			</TouchableOpacity>
